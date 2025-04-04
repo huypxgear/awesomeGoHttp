3532d8b3d79fdc2fd45ef2de1c743bbf75f3b6ec
Pham Xuan Huy <huypx@gearinc.com>
2025-04-04 15:24:52 +0700

    06-16 custom writer

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"os"
 )
 
+type logWriter struct {
+}
+
 func main() {
 	resp, err := http.Get("http://www.google.com")
 	if err != nil {
@@ -15,6 +18,13 @@ func main() {
 	}
 	// Prevent leak resource
 	defer resp.Body.Close()
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
 
-	io.Copy(os.Stdout, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+	return len(bs), nil
 }
